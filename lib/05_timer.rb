bff4d8a394983336937cd15d0198f9fa5e1ae44f
belldy <drieliluz@gmail.com>
2019-04-08 22:05:04 +0200

    update

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -3,25 +3,3 @@ def time_string(x)
 	return Time.at(x).utc.strftime("%H:%M:%S")
 end
 
-=begin
-seconds = 00
-
-def time_string(seconds)
-
-	if seconds < 60
-		return "00:00:#{seconds}"
-	elsif seconds > 60
-		minutes = seconds / 60
-		seconds2 = seconds - 60
-		return "00:#{minutes}:#{seconds2}" 
-	elsif seconds > 3600
-		hour = seconds / 3600
-		remain = 00
-		return "#{hour}:#{minutes}:#{seconds}" 
-	end
-
-end
-=end
-
-#if seconds > 60 && seconds < 1000
-
